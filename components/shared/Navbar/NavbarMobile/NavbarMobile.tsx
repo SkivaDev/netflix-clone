11dbee99576a7f7fb1974935608287941daf1b34
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-10-07 16:13:19 -0500

    feat(navbar): :sparkles: add navbar mobile

--- components/shared/Navbar/NavbarMobile/NavbarMobile.tsx
+++ components/shared/Navbar/NavbarMobile/NavbarMobile.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { Logo } from "@/components/shared/Logo";
+import { BellRing, Menu, Search } from "lucide-react";
+import { itemsNavbar } from "@/data/ItemsNavbar";
+import Link from "next/link";
 
 export const NavbarMobile = () => {
   return (
-    <div>NavbarMobile</div>
-  )
-}
+    <div className="p-4 flex justify-between">
+      <Logo />
+
+      <Sheet>
+        <SheetTrigger>
+          <Menu className="cursor-pointer" />
+        </SheetTrigger>
+        <SheetContent side={"left"} className="bg-black">
+          <div className="flex flex-col gap-4">
+            {itemsNavbar.map((item, index) => (
+              <Link key={index} href={item.link} className="hover:text-gray-300 transition-all duration-300">
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="border border-white/70 my-5"/>
+            <div className="flex justify-between gap-6 mt-4">
+                <Search className="cursor-pointer" />
+                <BellRing className="cursor-pointer" />
+                <p>User</p>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
