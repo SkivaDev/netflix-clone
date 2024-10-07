633fb37bd173f74c5e6da37242f2a0f62beeff6a
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-10-07 16:38:46 -0500

    feat(home): :sparkles: add SliderVideo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "@/components/shared/Navbar";
+import { SliderVideo } from "./(home)/components/SliderVideo";
 
 export default function Home() {
   return (
     <div className="relaive bg-zinc-900">
       <Navbar />
+      <SliderVideo />
 
       <div className="h-screen"/>
       <p>Section</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,7 +55,10 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		backgroundImage: {
+			'gradient-video': 'linear-gradient(180deg, hsla(0, 0%, 8%, 0), 0, hsla(0, 0%, 8%, 0.15) 15%, hsla(0, 0%, 8%, 0.35) 29%, hsla(0, 0%, 8%, 0.58) 44%, #171717 68%, #171717);'
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
