39318c26e88b04af4138c4820045159fc6965377
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-10-07 15:58:44 -0500

    feat(hooks): :sparkles: add useScrollPosition hook

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,20 @@ export default function Home() {
   return (
     <div className="relaive bg-zinc-900">
       <Navbar />
+
+      <div className="h-screen"/>
+      <p>Section</p>
+      <div className="h-screen"/>
+      <p>Section</p>
+      <div className="h-screen"/>
+      <p>Section</p>
+      <div className="h-screen"/>
+      <p>Section</p>
+      <div className="h-screen"/>
+      <p>Section</p>
+      <div className="h-screen"/>
+      <p>Section</p>
+      <div className="h-screen"/>
     </div>
   );
 }
--- components/shared/Navbar/NavbarDesktop/NavbarDesktop.tsx
+++ components/shared/Navbar/NavbarDesktop/NavbarDesktop.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { BellRing, Search } from "lucide-react";
 import React from "react";
 import { Logo } from "@/components/shared/Logo";
 import { itemsNavbar } from "@/data/ItemsNavbar";
 import Link from "next/link";
+import { useScrollPosition } from "@/hooks/useScrollPosition";
 
 export const NavbarDesktop = () => {
+
+  const scrollPosition = useScrollPosition();
+
+  console.log(scrollPosition);
+
   return (
     <div
       className={cn(
-        "z-30 left-0 right-0 top-0 h-16 w-full fixed transition-all duration-300"
+        "z-30 left-0 right-0 top-0 h-16 w-full fixed transition-all duration-300", scrollPosition > 20 ? "bg-black" : "bg-transparent"
       )}
     >
       <div className="px-[4%] mx-auto h-full">
