015abe374b23388688ab2c9c23b7e87ad4ab2614
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-10-17 22:46:59 -0500

    feat(project): :sparkles: add Auth.js and Prisma with Neon Tech

---
+++ auth.ts
@@ -0,0 +1,8 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { db } from "@/lib/db"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(db),
+  providers: [],
+})
