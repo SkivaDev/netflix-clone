2c1dde3df3489341b20b595425d1b6adae73d5b4
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-10-23 11:32:29 -0500

    feat(global components): :sparkles: add BlockMovies component

--- app/(home)/components/ListMovies/ListMovies.tsx
+++ app/(home)/components/ListMovies/ListMovies.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { ListMoviesProps } from './ListMovies.types'
+import { BlockMovies } from '@/components/shared/BlockMovies';
 
 export const ListMovies = (props: ListMoviesProps) => {
 
     const { movies } = props;
 
     return (
-    <div>ListMovies</div>
+    <div>
+        <BlockMovies title="Peliculas más recientes" movies={movies} isMyList={false} />
+    </div>
   )
 }
