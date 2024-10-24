569eddc6416cc7916fd18430d6ef9fd46e25757c
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-10-24 12:45:36 -0500

    feat(home): :sparkles: add favorite movies at Home page

--- app/(routes)/(home)/components/ListMovies/ListMovies.tsx
+++ app/(routes)/(home)/components/ListMovies/ListMovies.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import React from 'react'
 import { ListMoviesProps } from './ListMovies.types'
 import { BlockMovies } from '@/components/shared/BlockMovies';
+import { useLovedFilms } from '@/hooks/use-loved-films';
+import { useCurrentNetflixUser } from '@/hooks/use-current-user';
 
 export const ListMovies = (props: ListMoviesProps) => {
 
     const { movies } = props;
 
+    const { lovedFilmsByUser } = useLovedFilms();
+
+    const { currentUser  } = useCurrentNetflixUser();
+
+    const userNetflix = currentUser?.id;
+
+    const lovedFilms = userNetflix ? lovedFilmsByUser[userNetflix] : [];
+
     return (
     <div>
+        <BlockMovies title='Películas favoritas' movies={lovedFilms} isMyList={true} />
+
         <BlockMovies title="Peliculas más recientes" movies={movies} isMyList={false} />
     </div>
   )
